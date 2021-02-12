ffb745252a6ba1b3c3382cc20fcbd8c8a3e78028
Max <soudersmax@gmail.com>
2021-02-12 11:39:19 -0500

    folder refactor

--- search_compare.py
+++ search_compare.py
@@ -1,7 +1,7 @@
-from bin_search import bin_search
-from list_gen import num_list
-from lin_search import lin_search
-from timer import timed_function
+from lib.bin_search import bin_search
+from lib.list_gen import num_list
+from lib.lin_search import lin_search
+from lib.timer import timed_function
 import time
 
 
@@ -10,10 +10,7 @@ def run_search():
     length = int(input('How long do you want your list? '))
     
     l1 = num_list(ceil,length)
-    #l1 = [175, 190, 216, 405, 536, 749, 805, 1246, 1409, 1668, 1771, 1824, 2332, 2343, 
-    #      2624, 2727, 3172, 3244, 3325, 3406, 3841, 3910, 3969, 4040, 4044, 4375, 4531, 
-    #      5306, 5617, 5682, 5867, 5941, 6186, 6238, 6302, 6431, 6479, 6589, 6637, 6650,
-    #      7064, 7534, 7585, 8044, 8483, 8513, 8647, 8932, 8973, 9143, 9196, 9405, 9411, 9566, 9770]
+
     print(l1)
     
     target = int(input('What number do  you want to look for? '))
